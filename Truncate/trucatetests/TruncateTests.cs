bebec98e17566931e8336973ff9f9f53b8c4bbe7
Tiago Costa <nippius+github@gmail.com>
2017-09-05 13:56:07 +0100

    Add some initial tests

---
+++ Truncate/trucatetests/TruncateTests.cs
@@ -0,0 +1,49 @@
+﻿using libtruncate;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+
+namespace trucatetests
+{
+    [TestClass]
+    public class TruncateTests
+    {
+        [TestMethod]
+        public void TestFileIsntCreatedIfItDoesntExist()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestFileIsCreatedIfItDoesntExist()
+        {
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThrowsExceptionOnNegativeSize()
+        {
+            long negativeSize = -42;
+            string fileName = Path.GetRandomFileName();
+            List<string> files = new List<string>();
+            files.Add(fileName);
+
+            try
+            {
+                Truncate.TruncateFiles(FileMode.OpenOrCreate, false, negativeSize, files);
+            }
+            finally
+            {
+                new FileInfo(fileName).Delete();
+            }
+        }
+
+        [TestMethod]
+        public void TestNewFileSizeIsCorrect()
+        {
+
+        }
+    }
+}
